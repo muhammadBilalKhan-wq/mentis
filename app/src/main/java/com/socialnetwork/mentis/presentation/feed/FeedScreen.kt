c5258e9e77b57cf72b1181034455eaef12779f88
theelonberg <theelonberg@gmail.com>
2025-12-19 01:28:15 +0000

    16p

--- app/src/main/java/com/socialnetwork/mentis/presentation/feed/FeedScreen.kt
+++ app/src/main/java/com/socialnetwork/mentis/presentation/feed/FeedScreen.kt
@@ -24,8 +24,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.LoadState
-import androidx.paging.compose.collectAsLazyPagingItems
-import androidx.paging.compose.items
+import androidx.paging.compose.*
 import com.socialnetwork.mentis.R
 import com.socialnetwork.mentis.domain.model.Post
 
