a34882bddc99aa312c6cab07988e0a067ad662f4
theelonberg <theelonberg@gmail.com>
2025-12-20 04:12:50 +0000

    18b

---
+++ core/data/src/main/java/com/socialnetwork/mentis/core/data/di/NetworkModule.kt
@@ -0,0 +1,20 @@
+package com.socialnetwork.mentis.core.data.di
+
+import com.socialnetwork.mentis.core.data.remote.HttpClientFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): HttpClient {
+        return HttpClientFactory.create()
+    }
+}
