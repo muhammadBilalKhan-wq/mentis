b32ff629b017e99e2e2c0180e2a45d545dad1bed
theelonberg <theelonberg@gmail.com>
2025-12-20 04:46:41 +0000

    18c p

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.hilt.android)
+    alias(libs.plugins.kotlin.serialization)
     id("kotlin-kapt")
-    id("kotlinx-serialization")
 }
 
 android {
