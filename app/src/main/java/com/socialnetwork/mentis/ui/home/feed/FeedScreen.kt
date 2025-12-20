41f5f3ceb059620eb1075373e24a802c46c49a18
theelonberg <theelonberg@gmail.com>
2025-12-20 10:47:58 +0000

    20 p

--- app/src/main/java/com/socialnetwork/mentis/ui/home/feed/FeedScreen.kt
+++ app/src/main/java/com/socialnetwork/mentis/ui/home/feed/FeedScreen.kt
@@ -2,22 +2,23 @@ package com.socialnetwork.mentis.ui.home.feed
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.paging.compose.collectAsLazyPagingItems
+import com.socialnetwork.mentis.domain.model.Post
 import com.socialnetwork.mentis.ui.home.post.PostItem
-import androidx.paging.compose.items
 
 @Composable
-fun FeedScreen(
-    viewModel: FeedViewModel = hiltViewModel()
-) {
-    val feedPagingItems = viewModel.feedPagingData.collectAsLazyPagingItems()
+fun FeedScreen() {
+    val dummyPosts = listOf(
+        Post("1", "user1", "", "Caption 1", 10, 5, 0L),
+        Post("2", "user2", "", "Caption 2", 20, 10, 0L),
+        Post("3", "user3", "", "Caption 3", 30, 15, 0L)
+    )
 
     LazyColumn(modifier = Modifier.fillMaxSize()) {
-        items(feedPagingItems) { post ->
-            post?.let { PostItem(post = it) }
+        items(dummyPosts) { post ->
+            PostItem(post = post)
         }
     }
 }
