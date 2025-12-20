ed12e2b9e4785d5e4dded1b58af04b12c1bb2e42
theelonberg <theelonberg@gmail.com>
2025-12-20 09:40:22 +0000

    19 c p

--- app/src/main/java/com/socialnetwork/mentis/di/AppModule.kt
+++ app/src/main/java/com/socialnetwork/mentis/di/AppModule.kt
@@ -12,12 +12,10 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.DEFAULT
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
-import io.ktor.http.URLProtocol
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 import javax.inject.Singleton
@@ -46,12 +44,6 @@ object AppModule {
                 connectTimeoutMillis = TIMEOUT_MILLISECONDS
                 requestTimeoutMillis = TIMEOUT_MILLISECONDS
                 socketTimeoutMillis = TIMEOUT_MILLISECONDS
-            }
-            defaultRequest {
-                url {
-                    protocol = URLProtocol.HTTPS
-                    host = "my-json-server.typicode.com"
-                }
             }
         }
     }
