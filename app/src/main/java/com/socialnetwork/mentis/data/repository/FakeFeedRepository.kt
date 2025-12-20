3b6e342085bb2b8da2e1155e65a810dd3a44d05c
theelonberg <theelonberg@gmail.com>
2025-12-20 08:55:45 +0000

    19 c p

--- app/src/main/java/com/socialnetwork/mentis/data/repository/FakeFeedRepository.kt
+++ app/src/main/java/com/socialnetwork/mentis/data/repository/FakeFeedRepository.kt
@@ -1,26 +1,35 @@
 package com.socialnetwork.mentis.data.repository
 
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
 import androidx.paging.PagingData
-import com.socialnetwork.mentis.data.remote.FeedApi
-import com.socialnetwork.mentis.data.paging.PostPagingSource
 import com.socialnetwork.mentis.domain.model.Post
 import com.socialnetwork.mentis.domain.repository.FeedRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
 import javax.inject.Inject
 
-class FakeFeedRepository @Inject constructor(
-    private val feedApi: FeedApi
-) : FeedRepository {
+class FakeFeedRepository @Inject constructor() : FeedRepository {
 
     override fun getPosts(): Flow<PagingData<Post>> {
-        return Pager(
-            config = PagingConfig(
-                pageSize = 20,
-                enablePlaceholders = false
+        val fakePosts = listOf(
+            Post(
+                id = "1",
+                user = "Test User 1",
+                imageUrl = "",
+                caption = "Hello World!",
+                likes = 100,
+                comments = 12,
+                timestamp = 0
             ),
-            pagingSourceFactory = { PostPagingSource(feedApi) }
-        ).flow
+            Post(
+                id = "2",
+                user = "Test User 2",
+                imageUrl = "",
+                caption = "This is a test post.",
+                likes = 250,
+                comments = 34,
+                timestamp = 0
+            )
+        )
+        return flowOf(PagingData.from(fakePosts))
     }
 }
