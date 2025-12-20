6320323ddd31d5cf91c8efc88a6a6e22c805393c
theelonberg <theelonberg@gmail.com>
2025-12-20 10:43:32 +0000

    20 p

--- app/src/main/java/com/socialnetwork/mentis/ui/home/feed/FeedScreen.kt
+++ app/src/main/java/com/socialnetwork/mentis/ui/home/feed/FeedScreen.kt
@@ -1,15 +1,10 @@
 package com.socialnetwork.mentis.ui.home.feed
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.socialnetwork.mentis.ui.home.post.PostItem
 import androidx.paging.compose.items
@@ -23,56 +18,6 @@ fun FeedScreen(
     LazyColumn(modifier = Modifier.fillMaxSize()) {
         items(feedPagingItems) { post ->
             post?.let { PostItem(post = it) }
-        }
-
-        feedPagingItems.apply {
-            when {
-                loadState.refresh is LoadState.Loading -> {
-                    item {
-                        Box(
-                            modifier = Modifier.fillParentMaxSize(),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            CircularProgressIndicator()
-                        }
-                    }
-                }
-
-                loadState.append is LoadState.Loading -> {
-                    item {
-                        Box(
-                            modifier = Modifier.fillParentMaxSize(),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            CircularProgressIndicator()
-                        }
-                    }
-                }
-
-                loadState.refresh is LoadState.Error -> {
-                    val e = feedPagingItems.loadState.refresh as LoadState.Error
-                    item {
-                        Box(
-                            modifier = Modifier.fillParentMaxSize(),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            Text(text = "Error: ${e.error.localizedMessage}")
-                        }
-                    }
-                }
-
-                loadState.append is LoadState.Error -> {
-                    val e = feedPagingItems.loadState.append as LoadState.Error
-                    item {
-                        Box(
-                            modifier = Modifier.fillParentMaxSize(),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            Text(text = "Error: ${e.error.localizedMessage}")
-                        }
-                    }
-                }
-            }
         }
     }
 }
