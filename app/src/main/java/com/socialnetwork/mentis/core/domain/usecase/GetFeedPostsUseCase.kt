008de8fac2a335e7b4be809391db1aa337815dc5
broxroot <broxroot@gmail.com>
2025-12-21 07:38:10 +0000

    remove redundency p

--- app/src/main/java/com/socialnetwork/mentis/core/domain/usecase/GetFeedPostsUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.socialnetwork.mentis.core.domain.usecase
-
-import com.socialnetwork.mentis.core.domain.repository.FeedRepository
-import javax.inject.Inject
-
-class GetFeedPostsUseCase @Inject constructor(private val repository: FeedRepository) {
-    operator fun invoke() = repository.getFeedPosts()
-}
