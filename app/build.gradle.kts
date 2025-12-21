fb4cc8c8ac01e42c6306a4a8262017313d55d29c
broxroot <broxroot@gmail.com>
2025-12-21 10:08:20 +0000

    remove redundency p

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.compose.material:material-icons-extended:1.6.3")
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
