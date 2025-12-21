d12a138554bc5a357645d22082872f8681c2cb7b
broxroot <broxroot@gmail.com>
2025-12-21 13:45:03 +0000

    remove redundency p

--- app/src/main/java/com/socialnetwork/mentis/presentation/ui/feed/FeedFragment.kt
+++ app/src/main/java/com/socialnetwork/mentis/presentation/ui/feed/FeedFragment.kt
@@ -2,24 +2,36 @@ package com.socialnetwork.mentis.presentation.ui.feed
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.socialnetwork.mentis.databinding.FragmentFeedBinding
-import com.socialnetwork.mentis.presentation.ui.base.BaseFragment
+import com.socialnetwork.mentis.R
+import com.socialnetwork.mentis.presentation.ui.theme.MentisTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class FeedFragment : BaseFragment<FragmentFeedBinding>() {
+class FeedFragment : Fragment() {
 
     private val viewModel: FeedViewModel by viewModels()
 
-    override fun getViewBinding(
+    override fun onCreateView(
         inflater: LayoutInflater,
-        container: ViewGroup?
-    ) = FragmentFeedBinding.inflate(inflater, container, false)
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        // TODO: Use the ViewModel
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            id = R.id.compose_view
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+            setContent {
+                MentisTheme {
+                    FeedScreen(viewModel = viewModel)
+                }
+            }
+        }
     }
 }
