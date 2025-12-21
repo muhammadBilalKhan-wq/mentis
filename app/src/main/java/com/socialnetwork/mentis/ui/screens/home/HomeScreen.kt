8b6217354fe2b7636b9dfb442432f12e8e105a5a
broxroot <broxroot@gmail.com>
2025-12-21 07:42:22 +0000

    remove redundency p

--- app/src/main/java/com/socialnetwork/mentis/ui/screens/home/HomeScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.socialnetwork.mentis.ui.screens.home
-
-import androidx.compose.runtime.Composable
-import com.socialnetwork.mentis.presentation.feed.FeedScreen
-
-@Composable
-fun HomeScreen() {
-    FeedScreen()
-}
