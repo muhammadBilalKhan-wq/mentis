300a36104b83c9a1642e124b2a1405f4e0d60eb3
broxroot <broxroot@gmail.com>
2025-12-21 07:04:15 +0000

    remove redundency p

--- app/src/main/java/com/socialnetwork/mentis/di/AppModule.kt
+++ app/src/main/java/com/socialnetwork/mentis/di/AppModule.kt
@@ -2,11 +2,14 @@ package com.socialnetwork.mentis.di
 
 import android.app.Application
 import androidx.room.Room
+import com.socialnetwork.mentis.core.data.remote.FeedApi
 import com.socialnetwork.mentis.data.local.AppDatabase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -23,4 +26,18 @@ object AppModule {
         ).build()
     }
 
+    @Provides
+    @Singleton
+    fun provideFeedApi(retrofit: Retrofit): FeedApi {
+        return retrofit.create(FeedApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("http://10.0.2.2:8080")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 }
