6929181b8d8104bacfd87b4e27bf968845214a21
broxroot <broxroot@gmail.com>
2025-12-22 06:17:16 +0000

    new start

--- app/src/main/java/com/socialnetwork/mentis/domain/model/AppError.kt
+++ app/src/main/java/com/socialnetwork/mentis/domain/model/AppError.kt
@@ -1,6 +1,9 @@
 package com.socialnetwork.mentis.domain.model
 
-sealed class AppError(val message: String) {
-    class NetworkError(message: String) : AppError(message)
-    class UnknownError(message: String) : AppError(message)
+import com.socialnetwork.mentis.R
+import com.socialnetwork.mentis.core.ui.util.UiText
+
+sealed class AppError {
+    data class ApiException(val uiText: UiText) : AppError()
+    data class PostCreationException(val message: String) : AppError()
 }
